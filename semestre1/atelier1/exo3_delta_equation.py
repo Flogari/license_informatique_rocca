261d251a6b90c1ec3769829b4489f7cd9ccd0601
Flogari <54514003+Flogari@users.noreply.github.com>
2022-09-08 10:42:08 +0200

    Add files via upload

--- semestre1/atelier1/exo3_delta_equation.py
+++ semestre1/atelier1/exo3_delta_equation.py
@@ -1,22 +1,66 @@
 from math import sqrt
+import random
 
 def discriminant(a, b, c):
-    delta=b*b-4*a*c
-    return delta
+    """Permet de calculer delta
+
+    Args:
+        a (float): coefficient pour le x²
+        b (float): coefficient pour le x
+        c (float): coefficient directeur
+
+    Returns:
+        float: valeur de delta, le discriminant
+    """
+    return b*b-4*a*c
 
 def racine_unique(a,b):
-    x=-b/(2*a)
-    return x
+    """Permet de calculer la racine unique dans le cas delta=0
+
+    Args:
+        a (float): coefficient pour le x²
+        b (float): coefficient pour le x
+
+    Returns:
+        float: La racine unique, solution pour l'équation ax²+bx+c=0, avec delta=0
+    """
+    if a!=0:
+        return -b/(2*a)
+    else:
+        return 0
+
 
 def racine_double(a,b,delta,num):
-    if num == 1:
-        x1= (-b+sqrt(delta))/(2*a)
-        return x1
+    """Permet de calculer les racines dans le cas delta > 0
+
+    Args:
+        a (float): coefficient pour le x²
+        b (float): coefficient pour le x
+        delta (float): discriminant
+        num (float): Permet de choisir quel racine on veut calculer
+
+    Returns:
+        float: return une solution de l'équation dans le cas delta > 0
+    """
+    if a!=0:
+        if num == 1:
+            return (-b+sqrt(delta))/(2*a)
+        else:
+            return (-b-sqrt(delta))/(2*a)
     else:
-        x2= (-b-sqrt(delta))/(2*a)
-        return x2
+        return 0
 
 def str_equation(a, b, c):
+    """Permet de changer les différent coefficient en chaîne de caractère pour mettre sous la forme ax²+bx+c
+
+    Args:
+        a (float): coefficient pour le x²
+        b (float): coefficient pour le x
+        c (float): coefficient directeur
+
+    Returns:
+        str: return l'équation sous la fourme ax²+bx+c, pour plus de clarté pour l'utilisateur
+    """
     a = str(a)
     b = str(b)
     c = str(c)
@@ -24,6 +68,16 @@ def str_equation(a, b, c):
     return equation
 
 def solution_equation(a, b, c):
+    """fonction qui permet de données les solution de l'équation
+
+    Args:
+        a (float): coefficient pour le x²
+        b (float): coefficient pour le x
+        c (float): coefficient directeur
+
+    Returns:
+        str: return une chaîne de caractère qui donne les solution de l'équation de ax²+bx+c
+    """
     delta = discriminant(a, b, c)
     equation = str_equation(a, b, c)
     print("\n")
@@ -33,9 +87,15 @@ def solution_equation(a, b, c):
     if delta<0:
         equation = equation+"=0\nPas de racine réel, l'équation n'a pas de solution dans R"
     elif delta == 0:
-        equation = equation+"=0\nRacine unique\nx="+str(racine_unique(a, b))
+        if racine_unique(a, b) != 0:
+            equation = equation+"=0\nRacine unique\nx="+str(racine_unique(a, b))
+        else:
+            return "Erreur, a = 0, ce n'est pas une équation du second degrés"
     else:
-        equation = equation+"=0\nRacine double\nx1="+str(racine_double(a,b,delta,1))+"\nx2="+str(racine_double(a,b,delta,2))+"\n"
+        if (racine_double(a,b,delta,1) and racine_double(a,b,delta,2)) !=0:
+            equation = equation+"=0\nRacine double\nx1="+str(racine_double(a,b,delta,1))+"\nx2="+str(racine_double(a,b,delta,2))+"\n"
+        else:
+            return "Erreur, a = 0, ce n'est pas une équation du second degrés"
     return equation
 
 
@@ -47,3 +107,11 @@ c = input("c = \n")
 c = float(c)
 calcul = solution_equation(a, b, c)
 print(calcul)
+
+i=0
+for i in range(1, 10):
+    a=random.randint(2, 8)
+    b=random.randint(5, 11)
+    c=random.randint(2, 10)
+    reponse = solution_equation(a, b, c)
+    print(reponse)
--- semestre1/atelier1/exo4_date.py
+++ semestre1/atelier1/exo4_date.py
@@ -1,13 +1,28 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
-def date_est_valide (jour, mois, annee): #Vérification si la date est valide
+def date_est_valide (jour, mois, annee):
+    """Vérification si la date est valide
+
+    Args:
+        jour (int): jour de naissance
+        mois (int): mois de naissance
+        annee (int): année de naissance
+
+    Returns:
+        bool: return 0 ou 1 pour vérifier si la date est valide
+    """
     if (0<jour<31) and (0<mois<12):
         return 1
     else:
         return 0
 
-def saisie_date_de_naissance(): #L'utilisateur saisie sa date de naissance
+def saisie_date_de_naissance():
+    """Permet de saisir la date de naissance
+
+    Returns:
+        date: La date de naissance
+    """
     jour = input("Entrez une date en commençant par le jour \n jour = \n")
     jour = int(jour)
     mois = input("mois = \n")
@@ -19,12 +34,28 @@ def saisie_date_de_naissance(): #L'utilisateur saisie sa date de naissance
     else:
         return 0
 
-def age(date_naissance): #Calcul de l'âge
+def age(date_naissance):
+    """Calcul de l'âge en fonction de l'année de naissance
+
+    Args:
+        date_naissance (date): date de naissance de l'utilisateur
+
+    Returns:
+        int: l'âge de l'utilisateur
+    """
     today = date.today()
     jour = relativedelta(today, date_naissance)
     return jour.years
 
-def est_majeur(date_naissance): #Vérification de la majorité
+def est_majeur(date_naissance):
+    """Vérifie si l'utilisateur est majeur ou non
+
+    Args:
+        date_naissance (date): date de naissance de l'utilisateur
+
+    Returns:
+        bool: Renvoie si oui ou non, l'utilisateur est majeur
+    """
     #ages = age(date_naissance)
     today = date.today()
     jour = relativedelta(today, date_naissance)
@@ -33,6 +64,7 @@ def est_majeur(date_naissance): #Vérification de la majorité
         return True
     else:
         return False
+
 
 date_de_naissance = saisie_date_de_naissance() 
 if date_de_naissance != 0:   
